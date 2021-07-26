957fc9485902f01dcf75123d3e3e325e2d0a22ea
Keveluk <carlos.stefano@hotmail.com.br>
2021-07-26 18:40:36 -0300

    adiciona funcao remover da lista

--- src/components/MainClient/index.js
+++ src/components/MainClient/index.js
@@ -1,11 +1,11 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import * as S from './styled.js';
 import ClientForm from '../ClientForm/index.js';
-
+import hash from 'object-hash';
 
 function MainClient() {
 const [client, setClient] = useState({
-     id:0,
+  id:"",
     nome:"",
     endereco:"",
     cep:"",
@@ -14,6 +14,11 @@ const [client, setClient] = useState({
   })
 
   const [cadastros, setCadastros] = useState([])
+
+  useEffect(()=>{
+    attCadastro();
+  },[])
+
 
   function attCadastro(){
     let clientes = localStorage.getItem('clientes');
@@ -24,14 +29,19 @@ const [client, setClient] = useState({
   // }
   }
 
-
+  
+  function correctId(){
+    let clientes = localStorage.getItem('clientes');
+    let listaClientes = JSON.parse(clientes);
+    return listaClientes.length
+  }
   function salvar(){
-
+    client.id = hash(client);
     let clientes = localStorage.getItem('clientes');
     if(clientes === null){
       localStorage.setItem('clientes', JSON.stringify([client]));  
       setClient({
-      id:client.id+1,
+      id:"",
       nome:"",
       endereco:"",
       cep:"",
@@ -40,11 +50,10 @@ const [client, setClient] = useState({
     })
     }else{
       let listaClientes = JSON.parse(clientes);
-      listaClientes.push(client)
-      // localStorage.removeItem('clientes');  
+      listaClientes.push(client) 
       localStorage.setItem('clientes', JSON.stringify(listaClientes));  
       setClient({
-      id:client.id+1,
+      id:"",
       nome:"",
       endereco:"",
       cep:"",
@@ -57,7 +66,6 @@ const [client, setClient] = useState({
 
   function handleChange(e){
     if(e.target.getAttribute('name')==='nome'){
-      console.log(e.target.value)
       setClient({id:client.id,
         nome:e.target.value,
                 endereco:client.endereco,
@@ -102,6 +110,18 @@ setClient({id:client.id,
     }
   }
 
+    function deletar(e){
+      let clientes = localStorage.getItem('clientes');
+      let listaClientes = JSON.parse(clientes);
+      for (var i = 0; i < listaClientes.length; i++) {
+        if(listaClientes[i].id===e.target.id){
+          listaClientes.splice(i,1)
+          localStorage.setItem('clientes', JSON.stringify(listaClientes))
+          attCadastro()
+          return
+        }
+      }
+    }
 
   return (
     <>
@@ -128,8 +148,8 @@ setClient({id:client.id,
                   <S.ItemChild>
                   {cadastro.estado}
                   </S.ItemChild>
-                  <S.ItemChild>
-                  Delete
+                  <S.ItemChild onClick={deletar} id={cadastro.id}>
+                  <svg  aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path  fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
                   </S.ItemChild>
                   </S.Item>
               )
